74aee66c18561cc9e429f5168516d06554126a71
Benjamín <benjamin.abarca.c12@gmail.com>
2024-10-16 19:49:42 -0300

    microservicio online

--- src/components/ProductSearch.jsx
+++ src/components/ProductSearch.jsx
@@ -21,7 +21,7 @@ function ProductSearch({ onSearchResults, setLoading }) {
     }
   
     try {
-      const response = await fetch('http://localhost:5000/upload', {
+      const response = await fetch('https://ms-ia.tssw.cl/upload', {
         method: 'POST',
         body: formData,
       });
